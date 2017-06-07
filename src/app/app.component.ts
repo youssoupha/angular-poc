bcceb001944b139131fc79c6dfdabd1b1915167c
Youssoupha Sambe <yousamb@gmail.com>
2017-06-07 13:47:34 +0200

    feat: updating app-component to list heroes and presend details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
 
+const HEROES: Hero[] = [
+  { id: 1,  name: 'Windstorm' },
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  title = 'app works!';
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
 
 export class Hero {
