36b783b399a1b574c484e82d7aec96fc16dc0502
Youssoupha Sambe <yousamb@gmail.com>
2017-06-08 13:26:16 +0200

    refactor: reorganizing files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
-import {Hero, HEROES} from './model/hero';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './model/hero';
+import { HeroService } from './service/hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
-  heroes = HEROES;
+export class AppComponent implements OnInit {
+  heroes: Hero[];
   selectedHero: Hero;
 
+  constructor(private heroService: HeroService) {}
+  ngOnInit(): void {
+    this.fetchHeroes();
+  }
+  fetchHeroes(): void {
+    this.heroService.findHeroes().then(retrievedHeroes => this.heroes = retrievedHeroes);
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
