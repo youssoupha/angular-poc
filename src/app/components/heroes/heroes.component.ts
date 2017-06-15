5bd8f4a8815038967f1b59aa8239db0dcf5c1b70
Youssoupha Sambe <yousamb@gmail.com>
2017-06-15 17:27:00 +0200

    feat: adding route to detail into heroes view

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../../model/hero';
 import { HeroService } from '../../service/hero.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'my-heroes',
@@ -11,12 +12,18 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private router: Router
+  ) {}
   ngOnInit(): void {
     this.fetchHeroes();
   }
   fetchHeroes(): void {
     this.heroService.findHeroes().then(retrievedHeroes => this.heroes = retrievedHeroes);
+  }
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
